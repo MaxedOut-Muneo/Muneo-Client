3fd6dd900510df5cc41fc411952edb3312d35642
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-03-27 18:04:31 +0900

    chore: 프로젝트 초기 세팅

--- eslint.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import("eslint").Linter.Config[]} */
-export default [
-  {
-    ignores: ['.pnp.*', '.yarn/', 'node_modules/', 'dist/', '.next/', '.turbo/', 'storybook-static/'],
-  },
-];
