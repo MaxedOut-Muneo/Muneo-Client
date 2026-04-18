120c2433fd11764ff03e5d0a119a22a9d3515576
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-04-19 05:02:11 +0900

    feat: 랜딩 페이지 구현

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -1,11 +1,16 @@
-import { Button } from '@muneo/design-system';
+import { FaqSection } from './_components/FaqSection/FaqSection';
+import { HeroSection } from './_components/HeroSection/HeroSection';
+import { ServicesSection } from './_components/ServicesSection/ServicesSection';
+import { UsageGuideSection } from './_components/UsageGuideSection/UsageGuideSection';
+import * as styles from './page.css';
 
 export default function Home() {
   return (
-    <main>
-      <h1>문어</h1>
-      <p>문제 없는 시공을 위한 어시스턴트</p>
-      <Button>시작하기</Button>
+    <main className={styles.main}>
+      <HeroSection />
+      <ServicesSection />
+      <UsageGuideSection />
+      <FaqSection />
     </main>
   );
 }
