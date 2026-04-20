67516fefbe30e8c07e13c9ca8c89162cf08066e4
ksc <tmdcks1103@gachon.ac.kr>
2026-04-20 18:19:58 +0900

    chore: icon 추가

---
+++ packages/design-system/src/assets/icons/SearchIcon.tsx
@@ -0,0 +1,10 @@
+// @generated
+
+import type { SVGProps } from 'react';
+const SearchIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg" width={22} height={22} fill="none" {...props}>
+    <circle cx="10.0833" cy="10.0846" r="6.41667" stroke="currentColor" strokeWidth="2.5" />
+    <path d="M18.3333 18.332L15.5833 15.582" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" />
+  </svg>
+);
+export default SearchIcon;
