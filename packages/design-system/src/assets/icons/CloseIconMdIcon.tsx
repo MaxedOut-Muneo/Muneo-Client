e3144e02ee60ffdeb90b6c2b7cc89a0c632208d0
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-11 20:51:56 +0900

    chore: 아이콘 컴포넌트 재생성

--- packages/design-system/src/assets/icons/CloseIconMdIcon.tsx
+++ packages/design-system/src/assets/icons/CloseIconMdIcon.tsx
@@ -1,16 +1,15 @@
 // @generated
 
 import type { SVGProps } from 'react';
-const CircleWarningIcon = (props: SVGProps<SVGSVGElement>) => (
+const CloseIconMdIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24" {...props}>
     <path
-      stroke="#F59E0B"
+      stroke="#fff"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeOpacity={0.7}
-      strokeWidth={2}
-      d="M12 8.45v4M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18m.05-5.55v.1h-.1v-.1z"
+      strokeWidth={1.5}
+      d="m18 18-6-6m0 0L6 6m6 6 6-6m-6 6-6 6"
     />
   </svg>
 );
-export default CircleWarningIcon;
+export default CloseIconMdIcon;
