02ddbdeb02fd176c1dc7928f367282e58bff1c06
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-12 16:03:23 +0900

    chore: 디자인 시스템 신규 아이콘 추가

---
+++ packages/design-system/src/assets/icons/ArrowIcon.tsx
@@ -0,0 +1,9 @@
+// @generated
+
+import type { SVGProps } from 'react';
+const ArrowIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24" {...props}>
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m9 5 7 7-7 7" />
+  </svg>
+);
+export default ArrowIcon;
